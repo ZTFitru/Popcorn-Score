879d22e36416c7e7968d03bc31fc9558b13e00fa
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-08-26 22:10:26 -0400

    We deleted the test folder we created.
    Co-Authored-by: Charles <https://github.com/DRIF7ER>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,15 @@ import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import MovieDetails from '../MovieDetails/MovieDetails';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../Home/Home';
 import MovieDisplay from '../MovieDisplay/MovieDisplay';
+import Title from '../Title/Title';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path='/' element={<Title />}/>
         <Route path='/movies/:id' element={<MovieDetails />}/>
         <Route path='/movies/:movie_id/videos' element={<MovieDisplay />}/>
       </Routes>
